cf322b1fa10cd2028e57cace6b970f68bac04582
minzilov <min22zzang98@gmail.com>
2020-08-30 22:40:16 +0900

    we are ready after loading

--- src/App.js
+++ src/App.js
@@ -2,43 +2,19 @@ import React from 'react';
 import PropTypes from "prop-types";
 
 class App extends React.Component{
-  constructor(props){
-    super(props);
-    console.log("hello");
-  }
-  // state is obect that data we will change and why we are going to use
-  state ={
-    count : 0
-  };
-
-  // setState is going to call render
-  add=()=>{
-    this.setState(current => ({count : current.count +1}));
-  };
-  minus=()=>{
-    this.setState(current => ({count : current.count -1}));
-  };
-
-  componentDidMount(){
-    console.log("component rendered");
-  }
-  componentDidUpdate(){
-    console.log("I just updated");
-  }
-  componentWillMount(){
-    console.log("Goodbye, cruel world");
-  }
-  // class compenent need to 'render'
-  render(){
-  console.log("I'm rendering");
-  return(
-    <div>
-      <h1>The number is : {this.state.count}</h1>
-      <button onClick={this.add}>Add</button>
-      <button onClick={this.minus}>Minus</button>
-    </div>
-  )
-  }
+   state={
+     isLoading : true,
+     moives:[]
+   };
+   componentDidMount(){
+     setTimeout(()=>{
+       this.setState({isLoading : false,});
+     }, 6000);
+   }
+   render(){
+    const{ isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
+   }
 }
 
 export default App;
