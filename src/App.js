45b96ede0cc059a9197be8e44e0a95962b27b69c
minzilov <min22zzang98@gmail.com>
2020-08-30 22:29:45 +0900

    function of react

--- src/App.js
+++ src/App.js
@@ -1,77 +1,44 @@
 import React from 'react';
 import PropTypes from "prop-types";
 
-const foodILike=[
-  {
-    id:1,
-    name: "Kimchi",
-    image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
-    rating : 5
-  },
-  {
-    id:2,
-    name: "Samgyeopsal",
-    image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
-    rating : 4.8
-  },
-  {
-    id:3,
-    name: "Bibimbap",
-    image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
-    rating : 4.3
-  },
-  {
-    id:4,
-    name: "Doncasu",
-    image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
-    rating : 3
-  },
-  {
-    id:5,
-    name: "Kimbap",
-    image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
-    rating : 2.7
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    console.log("hello");
   }
-];
+  // state is obect that data we will change and why we are going to use
+  state ={
+    count : 0
+  };
 
-function Food({name, picture, rating}){
-  // console.log(props.fav);
-  //props means that what you want to input
-  return (<div>
-    <h2>I like {name}</h2>
-    <h4>{rating}/5.0</h4>
-    <img src={picture} alt={name}/>
-  </div>
-  );
-}
+  // setState is going to call render
+  add=()=>{
+    this.setState(current => ({count : current.count +1}));
+  };
+  minus=()=>{
+    this.setState(current => ({count : current.count -1}));
+  };
 
-Food.propTypes={
-   name : PropTypes.string.isRequired,
-   picture : PropTypes.string.isRequired,
-   rating : PropTypes.number.isRequired
-}
-
-function renderFood(dish){
-  console.log(dish);
-  return <Food name={dish.name} picture={dish.image} />
-}
-
-function App() {
+  componentDidMount(){
+    console.log("component rendered");
+  }
+  componentDidUpdate(){
+    console.log("I just updated");
+  }
+  componentWillMount(){
+    console.log("Goodbye, cruel world");
+  }
+  // class compenent need to 'render'
+  render(){
+  console.log("I'm rendering");
   return(
     <div>
-      {/* {console.log(foodILike.map(renderFood))}
-      {foodILike.map(renderFood)} */}
-      {/* dish is just like a object */}
-      {foodILike.map(dish => 
-      (<Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/> 
-      ))}
+      <h1>The number is : {this.state.count}</h1>
+      <button onClick={this.add}>Add</button>
+      <button onClick={this.minus}>Minus</button>
     </div>
-    );
+  )
+  }
 }
 
 export default App;
