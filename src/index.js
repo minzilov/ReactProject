9ce5ef60bacfcdd34828169a12fb92aa70259715
minzilov <min22zzang98@gmail.com>
2020-08-31 00:32:28 +0900

    movies types

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-
-
-ReactDOM.render(
-// component : function that return HTML
-  <App />,
-  document.getElementById("potato")
-);
+ReactDOM.render(<App />, document.getElementById("potato"));
 
 
 
