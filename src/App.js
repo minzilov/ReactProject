03390bbf85fd403b26aeae0e05ead84cb4d54452
minzilov <min22zzang98@gmail.com>
2020-09-01 11:38:27 +0900

    add css and summary

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import Movie from "./Movie";
+import "./App.css";
 
 class App extends React.Component {
   state = {
@@ -14,7 +15,7 @@ class App extends React.Component {
       }
     } = await axios.get(
       "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
-    );
+    )
     this.setState({ movies, isLoading: false });
   };
   componentDidMount() {
@@ -23,10 +24,14 @@ class App extends React.Component {
   render() {
     const { isLoading, movies } = this.state;
     return (
-      <div>
-        {isLoading
-          ? "Loading..."
-          : movies.map(movie => (
+    <section className="container">
+      {isLoading ? (
+      <div className="loader">
+        <span className="loader__text">Loading...</span>
+      </div>
+    ): (
+      <div className="movies">
+         {movies.map(movie => (
               <Movie
                 key={movie.id}
                 id={movie.id}
@@ -34,9 +39,11 @@ class App extends React.Component {
                 title={movie.title}
                 summary={movie.summary}
                 poster={movie.medium_cover_image}
-              />
-            ))}
+                genres={movie.genres}
+              />))};
       </div>
+    )}
+      </section>
     );
   }
 }
